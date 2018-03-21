be2d39f5a45bf283b3a12a49ea4d8dbccc07ac31
billhoeppner <billhoeppner@2015.tbst.edu.au>
2018-03-21 17:56:51 +0800

    Edit script.

--- setssrjson.sh
+++ setssrjson.sh
@@ -29,17 +29,18 @@ then
     echo -e "json file existed."
     if grep -q '"server_port":' /etc/shadowsocks-r/config.json
     then
-        port=cat /etc/shadowsocks-r/config.json | grep '"server_port":' | grep -o '[0-9]\+'
-        echo -e "port is $port"
+        port=`cat /etc/shadowsocks-r/config.json | grep '"server_port":' | grep -o '[0-9]\+'`
+        echo -e "port is $port."
         set_json
         if grep -q $port /etc/shadowsocks-r/config.json
         then
-            echo -e "json file edit successfully."
+            echo -e "json file edited successfully."
+            /etc/init.d/shadowsocks-r restart
         else
-            echo -e "[${red}Error${plain}] edit jason file failed."
+            echo -e "[${red}Error${plain}] Edit jason file failed."
         fi
     elif grep -q '"port_password":' /etc/shadowsocks-r/config.json
     then
-        echo -e "[${red}Error${plain}] The json file has been edited already."
+        echo -e "[${red}Error${plain}] The json file has already been edited."
     fi
 fi
