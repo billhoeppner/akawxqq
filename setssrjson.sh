d7ba15e76a37810ec3cba10ae20c624d9835de8a
billhoeppner <billhoeppner@2015.tbst.edu.au>
2018-03-21 16:18:39 +0800

    Edit json.

---
+++ setssrjson.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+if [ -f /etc/shadowsocks-r/config.json ]
+then
+    if grep -Fxq'"server_port":' /etc/shadowsocks-r/config.json
+    then
+        port=cat /etc/shadowsocks-r/config.json | grep '"server_port":' | grep -o '[0-9]\+'
+    elif grep -Fxq '"port_password":' /etc/shadowsocks-r/config.json
+    then
+        echo -c "[${red}Error${plain}] The json file has been edited already."
